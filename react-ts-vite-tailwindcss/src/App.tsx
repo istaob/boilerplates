4467017244b17f0018c9b206dde51b053b9f052f
t.yang <t.yang@Tao-Yang-Young.local>
2023-04-25 15:31:10 +0800

    feat: 🎸 new react-ts-vite-tailwindcss boilerplates

---
+++ react-ts-vite-tailwindcss/src/App.tsx
@@ -0,0 +1,12 @@
+import { ReactComponent as ReactLogo } from '@/assets/react.svg';
+
+function App() {
+  return (
+    <div className="w-6/12 mx-auto py-24 flex flex-col items-center">
+      <ReactLogo />
+      <h1 className="mt-6">Vite + React + TypeScript + Tailwind CSS</h1>
+    </div>
+  );
+}
+
+export default App;
