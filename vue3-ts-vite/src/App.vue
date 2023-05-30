54ec4d40d9a978878be82f5524c3389a9c2a55cd
t.yang <t.yang@Tao-Yang-Young.local>
2023-05-30 18:10:43 +0800

    feat: 🎸 add vue-ts-vite template

---
+++ vue3-ts-vite/src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="wrapper">
+    <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    <div>vue 3 + vite 4 + typescript 5</div>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+  padding-top: 100px;
+}
+.logo {
+  height: 32px;
+}
+</style>
